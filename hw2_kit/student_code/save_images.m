e25fac966f4f35e70b81e4df2bbbeb8d3bcc442b
klaywittler <klaywittler@gmail.com>
2019-02-06 13:43:56 -0500

    processed images

--- hw2_kit/student_code/save_images.m
+++ hw2_kit/student_code/save_images.m
@@ -7,13 +7,13 @@ function [] = save_images( images, im_name)
 % Written for the University of Pennsylvania's Robotics:Perception course
 
 if nargin==1
-    im_name = 'warped_img';
+    im_name = 'processed_img';
 end
 
 num_ima = length(images);
 
 for i=1:num_ima
-    imwrite(images{i}, ['warped_images/' im_name num2str(i) '.png']);
+    imwrite(images{i}, ['processed_images/' im_name num2str(i) '.png']);
 end
 
 end
